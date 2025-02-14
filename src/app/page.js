192cf4dad8169f3502808a1f4e82033308c5cd80
Spagum <acrunchydesign@gmail.com>
2025-02-15 00:05:21 +1100

    3rd commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import BackgroundVideo from "@/app/components/BackgroundVideo";
+import BackgroundVideo from "@/app/components/BackgroundVideo"
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function Home() {
@@ -18,16 +19,17 @@ export default function Home() {
       <div>
         <img src="vcilogo2.png" alt="logo" className="absolute top-4 left-4 w-20 md:w-24 lg:w-24 h-auto" />
 
-
-
         <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-gray-200 px-10 ">
           <h1 className="text-4xl sm:text-4xl  md:text-5xl lg:text-6xl font-bold py-8">coming soon.</h1>
           <hr className="w-8 border-t-2 border-gray-300 my-2 mx-auto" />
           <h3 className="text-lg sm:text-lg md:text-3xl lg:text-3xl font-semibold py-10 text-center">where student founders build the future</h3>
         </div>
-
-
       </div>
+
+
+      <Analytics />
+
     </div >
+
   )
 }
